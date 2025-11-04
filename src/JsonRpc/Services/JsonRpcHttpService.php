44b79367580d5102df2815e36638b864fb67c589
jmiy <751399695@qq.com>
2025-11-04 12:35:39 +0800

    添加  rpc   模块

--- src/JsonRpc/Services/JsonRpcHttpService.php
+++ src/JsonRpc/Services/JsonRpcHttpService.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
 
-namespace App\JsonRpc\Services;
+namespace Business\Hyperf\JsonRpc\Services;
 
 use Hyperf\RpcServer\Annotation\RpcService;
 
