5b4d17ccde4dc2671789eb70d6a2922dc70be815
jmiy <751399695@qq.com>
2025-12-11 09:38:38 +0800

    注释  调试  代码

--- src/Utils/Service/QueueService.php
+++ src/Utils/Service/QueueService.php
@@ -74,7 +74,7 @@ class QueueService
      */
     public static function push($params, string $queue = 'default', int $delay = 0): bool
     {
-        var_dump(__METHOD__, func_get_args());
+//        var_dump(__METHOD__, func_get_args());
         // 这里的 `PublicJob` 会被序列化存到 Redis 中，所以内部变量最好只传入普通数据
         // 同理，如果内部使用了注解 @Value 会把对应对象一起序列化，导致消息体变大。
         // 所以这里也不推荐使用 `make` 方法来创建 `Job` 对象。
